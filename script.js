8d5add6a33c0565698c1cff3c2c09b43f30c9eec
Kashish Singh Jaiswal <kashishsinghjaiswal4@gmail.com>
2025-07-16 09:40:48 +0530

    image

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-// Typewriter
-const typewriter = document.getElementById('typewriter');
-if (typewriter) {
-  new Typewriter(typewriter, {
-    strings: ['Hi, I\'m Kashish.', 'Full Stack Developer.'],
-    autoStart: true,
-    loop: true,
-  });
-}
+AOS.init();
 
-// Back to Top
-const backToTop = document.getElementById("back-to-top");
-window.onscroll = () => {
-  backToTop.style.display = window.scrollY > 300 ? "block" : "none";
-};
-backToTop.onclick = () => {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-};
+  const typewriter = new Typewriter(document.getElementById('typewriter'), {
+    loop: true,
+    delay: 75,
+  });
+
+  typewriter
+    .typeString("Hi, I'm Kashish")
+    .pauseFor(1000)
+    .deleteAll()
+    .typeString("Full Stack Developer")
+    .pauseFor(1000)
+    .start();
